3a185d401946026e89d5a069b18be51e74639684
alen <alen.dunn1@gmail.com>
2022-02-10 15:32:09 -0500

    add alert if an invalid artist name is entered

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,39 +4,41 @@ var searchButtonEl = $("#search-btn");
 searchButtonEl.click(function(){
     artist = searchInputEl.val()
     getAttractionInfo(artist);
-})
+});
 
 function getAttractionInfo() {
     var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + artist + "&apikey=7oZcdUQaTEQX8dezNz2rq0vTFbDBmIoE"
     fetch(apiUrl).then(function(response){
         if (response.ok){
             response.json().then(function(data){
+                if(data.page.totalElements === 0){
+                    // this needs to be a modal instead of an alert window
+                    alert("No results found.")
+                }else{
                 displayAttraction(data);
                 console.log(data)
-            })
-        }
-    })
-}
+                console.log(data.page.totalElements)
+                }
+            });
+        };
+    });
+};
 
-// function getAttractionInfo(id){
-//     var apiUrl = "https://app.ticketmaster.com/discovery/v2/events/" + id + ".json?apikey=7oZcdUQaTEQX8dezNz2rq0vTFbDBmIoE"
-//     fetch(apiUrl).then(function(response){
-//         if (response.ok){
-//             response.json().then(function(data){
-//                 displayAttraction(data)
-//                 console.log(data)
-//             })
-//         }
-//     })
-// }
+function getArtistInfo(){
+
+};
+
+function displayArtistInfo(){
+
+};
 
 function displayAttraction(concertInfo){
     var cityName = concertInfo._embedded.events[0]._embedded.venues[0].city.name
-    $("#attraction-display").html(`<h2>${concertInfo._embedded.events[0].name}</h2>
+    $("#artist-concerts").html(`<h2>${concertInfo._embedded.events[0].name}</h2>
     <h3>Upcoming Concerts:</h3> 
-    <p>${concertInfo._embedded.events[0].name} in ${cityName} - 
+    <p>${concertInfo._embedded.events[0].dates.start.localDate} ${concertInfo._embedded.events[0].name} in ${cityName} - 
     <a href="${concertInfo._embedded.events[0].url}">Get Tickets</a></p>
-    <p>${concertInfo._embedded.events[1].name} in ${concertInfo._embedded.events[1]._embedded.venues[0].city.name} -
+    <p>${concertInfo._embedded.events[1].dates.start.localDate} ${concertInfo._embedded.events[1].name} in ${concertInfo._embedded.events[1]._embedded.venues[0].city.name} -
     <a href="${concertInfo._embedded.events[1].url}">Get Tickets</a></p>`)  
     
-}
+};
