09c5b83c99a2f51cb0a3e2881c1910aff34e6c2a
alen <alen.dunn1@gmail.com>
2022-02-16 23:34:32 -0500

    removed some comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var closeBtn = document.querySelector(".close-btn");
 var artistSearchEl = $("#artist-search");
 var searchedAritist = [];
 
-searchFormEl.submit(function(event){
+searchFormEl.submit(function (event) {
     event.preventDefault();
     artist = searchInputEl.val()
     getAttractionInfo(artist);
@@ -15,52 +15,51 @@ searchFormEl.submit(function(event){
 
 function getAttractionInfo(artist) {
     var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + artist + "&apikey=7oZcdUQaTEQX8dezNz2rq0vTFbDBmIoE"
-    fetch(apiUrl).then(function(response){
-        if (response.ok){
-            response.json().then(function(data){
-                if(data.page.totalElements === 0 || data.page.totalElements > 100){
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                if (data.page.totalElements === 0 || data.page.totalElements > 100) {
                     modal.style.display = "block";
                 }
-                else{
-                displayAttraction(data);
-                getArtistInfo(artist);
-                console.log(data)
+                else {
+                    displayAttraction(data);
+                    getArtistInfo(artist);
+                    console.log(data)
                 }
             });
         };
     });
 };
 
-function getArtistInfo(artist){
+function getArtistInfo(artist) {
     var apiUrl = "https://api.napster.com/v2.2/search?query=" + artist + "&type=artist&apikey=NGRhNzQ5MWEtZTUxNS00Mjk5LTk0YTYtYTI1YTMwN2ZkMGUw"
-    fetch(apiUrl).then(function(response){
-        if (response.ok){
-            response.json().then(function(data){
-                // console.log(data);
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
                 displayArtistInfo(data);
             })
         }
-        if (response.status === 400){
+        if (response.status === 400) {
             throw new Error('bad request')
         }
-    }).catch(function(error){
+    }).catch(function (error) {
         console.log(error)
     })
 
 };
 
-function displayArtistInfo(artistInfo){
+function displayArtistInfo(artistInfo) {
     var artistName = artistInfo.search.data.artists[0].name
     $("#artist-info").html(`<h2>${artistName}</h2>
     <img src="https://api.napster.com/imageserver/v2/artists/${artistInfo.search.data.artists[0].id}/images/230x153.jpg">`)
-  
+
 };
 
-function displayAttraction(concertInfo){
+function displayAttraction(concertInfo) {
     var concertListContainer = document.createElement("ul");
     var concertListEl = "";
     $("#artist-concerts").empty();
-    for (var i = 0; i < 5; i++){   
+    for (var i = 0; i < 5; i++) {
         var cityName = concertInfo._embedded.events[i]._embedded.venues[0].city.name
         var concertDate = concertInfo._embedded.events[i].dates.start.localDate
         var concertName = concertInfo._embedded.events[i].name
@@ -70,54 +69,52 @@ function displayAttraction(concertInfo){
 
     }
     concertListContainer.innerHTML = concertListEl
-    $("#artist-concerts").append(concertListContainer);  
+    $("#artist-concerts").append(concertListContainer);
 };
 
-closeBtn.onclick = function(){
+closeBtn.onclick = function () {
     modal.style.display = "none"
 };
 
-function saveArtists(artist){
+function saveArtists(artist) {
     search = document.createElement("li");
     if (searchInputEl.val() === "") {
         return;
-    } 
+    }
     else {
-    
-    search.innerHTML = "<a href='#' class='collection-item'>" + artist + "</a>"; 
-    artistSearchEl.append(search);
-    
-    searchedAritist.push(artist);
 
-    localStorage.setItem("searchedArtist", JSON.stringify(searchedAritist));
-    //clear input
-    // searchInputEl.val("");
-  }
+        search.innerHTML = "<a href='#' class='collection-item'>" + artist + "</a>";
+        artistSearchEl.append(search);
+
+        searchedAritist.push(artist);
+
+        localStorage.setItem("searchedArtist", JSON.stringify(searchedAritist));
+        searchInputEl.val("");
+    }
 }
 
-function displayArtistSearch(){
-    
+function displayArtistSearch() {
+
     var storedArtist = JSON.parse(localStorage.getItem("searchedArtist"));
-    
-    if (storedArtist === null){
+
+    if (storedArtist === null) {
         return;
-    }else{
+    } else {
 
         for (var i = 0; i < storedArtist.length; i++) {
             search = document.createElement("li");
             search.innerHTML = "<a href='#' class='collection-item'>" + storedArtist[i] + "</a>";
-
             artistSearchEl.append(search);
         }
-       
-        // artistSearchEl.append(artistList);
+
+
     }
-   
+
 }
 
 displayArtistSearch();
 
-var artistSaved = function(event) {
+var artistSaved = function (event) {
     var artist = event.target.innerText;
     getAttractionInfo(artist);
 }
